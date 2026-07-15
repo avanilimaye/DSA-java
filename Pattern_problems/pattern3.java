c058a8bc5a11522051d6572f40f7aba308ee1c45
Avani Limaye <avani.a.limaye@gmail.com>
2026-07-15 19:05:25 +0530

    pattern4

--- Pattern_problems/pattern3.java
+++ Pattern_problems/pattern3.java
@@ -4,15 +4,14 @@ class Main
     public static void main(String[] args)
     {
         int n=5;
-        for (int i=0;i<n;i++)
+        for(int i=0;i<n;i++)
         {
             for(int j=0;j<=i;j++)
             {
-                System.out.print(i+1 );
+                System.out.print(j+1);
             }
             System.out.println();
         }
-            
 
     }
 }
