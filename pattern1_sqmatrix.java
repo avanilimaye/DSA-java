f3370d00d72bdebd6dfc37e240d58efa96324576
Avani Limaye <avani.a.limaye@gmail.com>
2026-07-15 19:09:11 +0530

    Added output to code

---
+++ pattern1_sqmatrix.java
@@ -0,0 +1,19 @@
+package Pattern_problems;
+//take input n and print a square matrix n by n of stars
+
+public class pattern1_sqmatrix 
+{
+    public static void main(String[] args)
+    {
+        int n=5;
+        for (int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+             System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
---
+++ pattern2_pyramid.java
@@ -0,0 +1,16 @@
+package Pattern_problems;
+class pattern2_pyramid
+{//make a pyramid with increasing num of stars to n as go down.
+    public static void main (String[] args)
+    {
+        int n=5;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
