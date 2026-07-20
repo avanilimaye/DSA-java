fb56a61032e46f17fb55d4d89401157224cbac18
Avani Limaye <avani.a.limaye@gmail.com>
2026-07-20 12:23:08 +0530

    pattern19

---
+++ pattern19.java
@@ -0,0 +1,52 @@
+public class pattern19 
+{
+    public static void main(String[] args) 
+    {
+        int n=5;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=n-i;j>=1;j--)
+            {
+                System.out.print("*");
+            }
+            for(int k=0;k<i;k++)
+            {
+                System.out.print("  ");
+            }
+            for(int j=n-i;j>=1;j--)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            for(int k=i;k<n;k++)
+            {
+                System.out.print("  ");
+            }
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+/*
+**********
+****  ****
+***    ***
+**      **
+*        *
+*        *
+**      **
+***    ***
+****  ****
+**********
+*/
