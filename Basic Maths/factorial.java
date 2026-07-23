c8628a7d1d86340eed64059c216a794a30df696c
Avani Limaye <avani.a.limaye@gmail.com>
2026-07-23 17:30:54 +0530

    factorial

---
+++ Basic Maths/factorial.java
@@ -0,0 +1,19 @@
+public class factorial 
+{
+    public static void main(String[] args) 
+    {            
+        int n=5;
+        int fact=1;
+        for(int i=1;i<=n;i++)
+
+            {
+                fact=fact*i;
+            }
+            System.out.println(n+" Factorial:"+fact);
+
+    }
+}
+
+/*
+5 Factorial:120
+*/
