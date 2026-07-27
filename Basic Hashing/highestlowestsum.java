86d9e0f0eb2fb3a4fcb3cf72ed1a31d26e9f2dfb
Avani Limaye <avani.a.limaye@gmail.com>
2026-07-27 21:31:03 +0530

    highestlowest freq sum

--- Basic Hashing/highestlowestsum.java
+++ Basic Hashing/highestlowestsum.java
@@ -5,8 +5,7 @@ public class highestlowestsum
 {
     public static void main(String[] args) 
     {
-        
-        
+        int[] arr={1,2,2,3,3,3,1,3,2,1,2,3,2,1,3,3,3,2,1,1};
         int n=arr.length;
 
         //find max element for hash array length
