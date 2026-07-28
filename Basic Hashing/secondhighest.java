1dd48a0075accc580b92c072be67c7b3bde4d7af
Avani Limaye <avani.a.limaye@gmail.com>
2026-07-28 15:25:03 +0530

    secondhighest

---
+++ Basic Hashing/secondhighest.java
@@ -0,0 +1,52 @@
+
+public class secondhighest 
+{
+    public static void main(String[] args) 
+    {
+        int arr[]={9,9,9,9,9,8,8,8,8,7,7,7};
+        int n=arr.length;
+        int arrmax=arr[0];
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]>arrmax)
+            {
+                arrmax=arr[i];
+            }
+        }
+
+        int hash[]=new int[arrmax+1];
+        for(int i=0;i<n;i++)
+        {
+            int a =arr[i];
+            hash[a]++;
+        }
+    
+    int highestfreq=0;
+    int highestelement=0;
+    int secondhighest=0;
+    int secondfreq=0;
+    for(int j=0;j<hash.length;j++)
+    {
+        if(hash[j]>highestfreq)
+        {
+            secondfreq=highestfreq;
+            secondhighest=highestelement;
+
+            highestfreq=hash[j];
+            highestelement=j;
+        }
+        else if(hash[j]>secondhighest && hash[j]<highestfreq)
+        {
+            secondfreq=hash[j];
+            secondhighest=j;
+        }
+    }
+    System.out.println("second highest element:"+secondhighest);
+    System.out.println("its frequency:"+secondfreq);
+}
+}
+/*
+int arr[]={9,9,9,9,9,8,8,8,8,7,7,7};
+second highest element:8
+its frequency:4
+*/
