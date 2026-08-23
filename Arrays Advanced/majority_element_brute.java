b96ab5f1349ec29788f7949bcf0f0e258bfa1b66
Avani Limaye <avani.a.limaye@gmail.com>
2026-08-23 13:58:22 +0530

    majority_element_brute

---
+++ Arrays Advanced/majority_element_brute.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class majority_element_brute 
+{
+    public static void main(String[] args) 
+    {
+        int [] nums={1,3,2,7,3,3,4,6,3,4,3,3,3};
+        for(int i=0;i<nums.length;i++)
+        {
+            int counter=0;
+            for(int j=0;j<nums.length;j++)
+            {
+                if(nums[i]==nums[j])
+                {
+                    counter++;
+                }
+            }
+            if(counter>nums.length/2)
+            {
+                System.out.println("majority:"+nums[i]);
+                return ;
+            }
+        }
+        System.out.println("no majority");
+    }
+}
